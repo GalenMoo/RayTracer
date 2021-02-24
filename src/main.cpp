725d040f2a9b116a12a5ba08196e60ddf37b819e
GalenMoo <wu.galen@gmail.com>
2021-02-24 08:43:05 -0800

    faster run time

--- src/main.cpp
+++ src/main.cpp
@@ -83,19 +83,20 @@ void Render(){
     const int max_depth = 50;
 
     //World Setup
-    Hittable_List world = random_scene();
+    // Hittable_List world = random_scene();
 
     // //1
-    // auto material_ground = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
-    // auto material_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-    // auto material_left   = std::make_shared<Dielectric>(1.5);
-    // auto material_right  = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.0);
+    Hittable_List world;
+    auto material_ground = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
+    auto material_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
+    auto material_left   = std::make_shared<Dielectric>(1.5);
+    auto material_right  = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.0);
 
-    // world.add(std::make_shared<Sphere>(Point( 0.0, -100.5, -1.0), 100.0, material_ground));
-    // world.add(std::make_shared<Sphere>(Point( 0.0,    0.0, -1.0),   0.5, material_center));
-    // world.add(std::make_shared<Sphere>(Point(-1.0,    0.0, -1.0),   0.5, material_left));
-    // world.add(std::make_shared<Sphere>(Point(-1.0,    0.0, -1.0),  -0.4, material_left));
-    // world.add(std::make_shared<Sphere>(Point( 1.0,    0.0, -1.0),   0.5, material_right));
+    world.add(std::make_shared<Sphere>(Point( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(std::make_shared<Sphere>(Point( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(std::make_shared<Sphere>(Point(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(std::make_shared<Sphere>(Point(-1.0,    0.0, -1.0),  -0.4, material_left));
+    world.add(std::make_shared<Sphere>(Point( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
     Point lookfrom(13,2,3);
